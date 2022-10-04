1caff6fbf54f039c6baba708e896d69ecb36db10
Malith Ranasinghe <malith.ranasinghe@arimaclanka.com>
2022-10-05 01:45:28 +0530

    build: storybook update

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 import * as NextImage from 'next/image';
-import '../styles/globals.css';
+import '../pages/globals.css';
 
 const BREAKPOINTS_INT = {
   xs: 375,
@@ -42,4 +42,8 @@ export const parameters = {
     },
   },
   viewport: { viewports: customViewports },
+  layout: 'fullscreen',
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
 };
